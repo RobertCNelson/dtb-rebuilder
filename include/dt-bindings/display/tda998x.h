540d75f21b820936b7033aafb4cd7da35a6b3597
Robert Nelson <robertcnelson@gmail.com>
2016-10-31 16:30:49 -0500

    4.9-rc3-ti-r0
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- include/dt-bindings/display/tda998x.h
+++ include/dt-bindings/display/tda998x.h
@@ -1,7 +1,7 @@
 #ifndef _DT_BINDINGS_TDA998X_H
 #define _DT_BINDINGS_TDA998X_H
 
-#define AFMT_SPDIF	1
-#define AFMT_I2S	2
+#define TDA998x_SPDIF	1
+#define TDA998x_I2S	2
 
 #endif /*_DT_BINDINGS_TDA998X_H */
